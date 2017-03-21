dfdc4e3fc831e4178d36dd37980e3d79f7c9cad0
Dan Lauby and Erica Wright <erica@>
2017-03-21 12:03:12 -0700

    display API info on index and create news-tile component

--- app/models/news-article.js
+++ app/models/news-article.js
@@ -1,7 +1,13 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  // title: DS.attr(),
+  // article: DS.attr(),
+  // date: DS.attr()
+  author: DS.attr(),
   title: DS.attr(),
-  article: DS.attr(),
-  date: DS.attr()
+  description: DS.attr(),
+  URL: DS.attr(),
+  imageURL: DS.attr(),
+  publishedAt: DS.attr()
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,11 @@ import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // actions: {
+  //   zipLookup(params) {
+  //     this.sendAction('results', params.zip);
+  //   }
+  // }
   // model() {
   //   return this.store.findAll('news-article');
   // }
@@ -10,8 +15,10 @@ export default Ember.Route.extend({
     var key = config.myApiKey;
     var url = 'https://newsapi.org/v1/articles?source=national-geographic&sortBy=top&apiKey=' + key;
     return Ember.$.getJSON(url).then(function(responseJSON) {
+      //the API call returns data in an array called responseJSON
       console.log(responseJSON);
-      return responseJSON.results;
+      //the articles need returned to be displayed on page, using .articles which is the array name in the response
+      return responseJSON.articles;
     });
   }
 
